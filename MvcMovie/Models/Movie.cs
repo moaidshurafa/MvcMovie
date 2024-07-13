069ea77ef8f35b7ef881a3ec9c030739851c2dc1
User <User@DESKTOP-S3PEIBB>
2024-07-13 20:34:07 +0300

    Add project files.

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        [Display(Name = "Release Date")]
+
+        [DataType(DataType.Date)]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+    }
+}
